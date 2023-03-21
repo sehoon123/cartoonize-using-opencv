ae6bd6a3738d3073727f1679de97a9e89c208f2e
정세훈 <sehoon@jeongsehun-ui-noteubug.local>
2023-03-21 16:34:33 +0900

    canny 2.0

--- canny_like.py
+++ canny_like.py
@@ -1,13 +1,17 @@
 import cv2
 
-# Read the image
+# Read the image and downscale it
 img = cv2.imread('image.jpg')
+img = cv2.resize(img, None, fx=0.5, fy=0.5)
 
 # Apply a bilateral filter to the image
 filtered = cv2.bilateralFilter(img, 9, 75, 75)
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(filtered, cv2.COLOR_BGR2GRAY)
+# Apply a median blur to the filtered image
+blurred = cv2.medianBlur(filtered, 9)
+
+# Convert the blurred image to grayscale
+gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
 # Apply an adaptive threshold to the grayscale image
 threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
@@ -15,8 +19,12 @@ threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THR
 # Apply the Canny edge detection algorithm to the thresholded image
 edges = cv2.Canny(threshold, 100, 200)
 
+# Dilate the edges to make them thicker
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+dilated = cv2.dilate(edges, kernel, iterations=2)
+
 # Merge the edge-detected image with the original image using bitwise AND operator
-cartoon = cv2.bitwise_and(img, img, mask=edges)
+cartoon = cv2.bitwise_and(img, img, mask=dilated)
 
 # Convert the merged image to grayscale and invert it to create a black and white effect
 bw_cartoon = cv2.cvtColor(cartoon, cv2.COLOR_BGR2GRAY)
